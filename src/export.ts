2f13f536fd2beebb87b0e31708aa862ecf4357ec
Lucas Godtfredsen de Souza <lucasgodt@gmail.com>
2020-05-19 12:24:44 -0300

    Add files via upload

--- src/export.ts
+++ src/export.ts
@@ -5,7 +5,7 @@ import * as admin from 'firebase-admin';
  * Suggestion from jcummings2 and leningsv
  * @param {Array<string>} collectionNameArray
  */
-export const getAllCollections = (collectionNameArray): Promise<any> => {
+export var getAllCollections = (collectionNameArray): Promise<any> => {
   const db = admin.firestore();
   // get all the root-level paths
   return new Promise(resolve => {
@@ -38,24 +38,24 @@ export const getAllCollections = (collectionNameArray): Promise<any> => {
  * @param {string} collectionName
  * @returns {Promise<any>}
  */
-export const backup = async (collectionName: string): Promise<any> => {
+export var backup = async (collectionName: string): Promise<any> => {
   try {
     const db = admin.firestore();
     let data = {};
     data[collectionName] = {};
 
-    const documents = await db.collection(collectionName).get();
+    var documents = await db.collection(collectionName).get();
 
-    for (const doc of documents.docs) {
-      const subCollections = await doc.ref.listCollections();
+    for (var doc of documents.docs) {
+      var subCollections = await doc.ref.listCollections();
 
       data[collectionName][doc.id] = doc.data();
 
-      for (const subCol of subCollections) {
-        const subColData = await backup(
+      for (var subCol of subCollections) {
+        var subColData = await backup(
           `${collectionName}/${doc.id}/${subCol.id}`
         );
-        data[collectionName][doc.id]['subCollection'] = subColData;
+        data[collectionName][doc.id][subCol.id] = subColData;
       }
     }
 
